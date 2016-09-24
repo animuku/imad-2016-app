b7f112965b3bfb545d56cdb0d78a7d1b52dc3f70
animuku <animuku123@gmail.com>
2016-09-24 17:25:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,9 @@
-//Counter code
-var button=document.getElementById('counter');
+var button= document.getElementById('counter');
+var counter=0;
 
-button.onclick=function(){
-    //Create a request
-    var request= new XMLHttpRequest();
+button.onclick= function(){
     
-    
-    //Capture the response and store it in a variable
-    
-    request.onreadystatechange= function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-        //Take some action
-            if(request.status===200){
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-                
-            }
-        }
-    };
-    
-    //Make the request
-    request.open('GET', 'http://animuku.imad.hasura-app.io/counter', true);
-    request.send(null);
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
     
 };
